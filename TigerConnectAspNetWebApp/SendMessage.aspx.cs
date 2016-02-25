c87ba70eb52c6c107b53068bac7e115779359702
Jeff Cook <jeff@tigertext.com>
2016-02-25 02:16:12 -0800

    Remove the old website project type and all associated files from the solution Implement the bootstrap template in the new web application project that will be used going forward Put a reference to the /packages/TTWinSDK.dll for now, until this becomes a NuGet package.

--- TigerConnectAspNetWebApp/SendMessage.aspx.cs
+++ TigerConnectAspNetWebApp/SendMessage.aspx.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-
+﻿using System.Collections.Generic;
+using System;
+using System.Configuration;
+using TT.Win.SDK;
+using TT.Win.SDK.Api;
+using TT.Win.SDK.Model;
 
 namespace TigerConnectAspNetWebApp
 {
@@ -12,9 +11,56 @@ namespace TigerConnectAspNetWebApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            TT.Win.SDK.Global.Init("key", "secret");
-            TT.Win.SDK.Api.Message.SendMessage("body", "m");
+        }
 
+        protected void btnSend_Click(object sender, EventArgs e)
+        {
+            // Initialize the SDK using the API key and secret in the web.config file
+            var appSettings = ConfigurationManager.AppSettings;
+            Global.Init(appSettings["apiKey"], appSettings["apiSecret"]);
+
+            List<AttachedFile> filesToAttach = null;
+
+            if (FileUpload1.HasFile)
+            {
+                AttachedFile fileToAttach = new AttachedFile();
+                fileToAttach.FileContents = FileUpload1.FileBytes;
+                fileToAttach.FullFileName = FileUpload1.FileName;
+                filesToAttach = new List<AttachedFile>();
+                filesToAttach.Add(fileToAttach);
+            }
+
+            try
+            {
+                TT.Win.SDK.Model.MessageEventData messageSent = null;
+                if (string.IsNullOrEmpty(txtOrg.Text))
+                {
+                    messageSent = Message.SendMessage(this.txtMessage.Text, this.txtRecipient.Text, filesToAttach);
+                }
+                else
+                {
+                    messageSent = Message.SendMessage(this.txtMessage.Text, this.txtRecipient.Text, txtOrg.Text, filesToAttach);
+                }
+
+                if (messageSent.message_id != null)
+                {
+                    ShowResult(true, "Message sent successfully.  MessageId is " + messageSent.message_id);
+                }
+                else
+                {
+                    ShowResult(false, "Send Message failed.");
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowResult(false, ex.Message);
+            }
+        }
+
+        protected void ShowResult(bool success, string message)
+        {
+            string cssClass = success ? "alert alert-success" : "alert alert-danger";
+            sResults.Text = string.Format("<div class=\"{0}\">{1}</div>", cssClass, message);
         }
     }
 }

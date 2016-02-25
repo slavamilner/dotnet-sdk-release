90cdf454bb5fe7d103d3d46b834a873bf108f7db
Jeff Cook <jeff@tigertext.com>
2016-02-25 01:22:49 -0800

    Add the Web applicatoin project, see if this does better with pulls from github

---
+++ TigerConnectAspNetWebApp/SendMessage.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace TigerConnectAspNetWebApp
+{
+    public partial class SendMessage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TT.Win.SDK.Global.Init("key", "secret");
+            TT.Win.SDK.Api.Message.SendMessage("body", "m");
+
+        }
+    }
+}
